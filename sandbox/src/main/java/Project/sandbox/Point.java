69ccefedf5dbc5a6c172624fc039af49bbb0e89b
TatyanaKrupnova <40050093+TatyanaKrupnova@users.noreply.github.com>
2018-06-13 10:22:03 +0500

    Revert "Задание№2б пункт 4"
    This reverts commit 1d62dbf10c5d1ea7211e9916c60bfaf6640a7c93.

--- sandbox/src/main/java/Project/sandbox/Point.java
+++ sandbox/src/main/java/Project/sandbox/Point.java
@@ -3,14 +3,4 @@ package Project.sandbox;
 public class Point {
     double x;
     double y;
-
-    public Point(double x,double y){
-        this.x = x;
-        this.y = y;
-    }
-
-    public double distance(Point p1){
-        return Math.sqrt(((this.x - p1.x) * (this.x - p1.x)) + ((this.y - p1.y) * (this.y - p1.y)));
-    }
-
 }
