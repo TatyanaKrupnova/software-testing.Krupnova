b113a1e9a9c290b076451691c37244c7e556a95c
TatyanaKrupnova <40050093+TatyanaKrupnova@users.noreply.github.com>
2018-06-24 22:02:12 +0500

    создан базовы класс, в него перенесены методы

--- addressbook-web-tests/src/test/java/Project/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/Project/addressbook/ContactCreationTest.java
@@ -1,85 +1,15 @@
 package Project.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-public class ContactCreationTest {
-    FirefoxDriver wd;
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("X:/Soft/Firefox/firefox.exe"));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String user, String pass) {
-        wd.findElement(By.name("user")).sendKeys(user);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(pass);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+public class ContactCreationTest extends TestBase {
 
     @Test
     public void contactCreation() {
         addNewContact();
-        fillContactForm("Tatyana", "Krupnova", "Chelyabinsk", "9030883488", "1985");
+        enterContact();
         gotoHome();
     }
 
-    private void gotoHome() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void fillContactForm(String name, String surname, String whereYouLive, String phonenumber, String Year) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(name);
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(surname);
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(whereYouLive);
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(phonenumber);
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[21]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[21]")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[4]")).click();
-        }
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(Year);
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-     }
-
-    private void addNewContact() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
